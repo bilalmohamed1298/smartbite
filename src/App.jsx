62577db1c9fc6f65a00329432e560c2b97e82f48
Bilal Mourad <101932528+bilalmohamed1298@users.noreply.github.com>
2025-03-16 05:06:47 +0200

    Responsive Editing

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,16 @@ export default function App() {
       primary: {
         main: "#A34BCE",
       },
+      breakpoints: {
+        values: {
+          xs: 0,
+          sm: 600,
+          md: 900,
+          lg: 1200,
+          xl: 1536,
+          xxl: 1920,
+        },
+      },
     },
   });
   return (
--- src/Components/SignupForm.jsx
+++ src/Components/SignupForm.jsx
@@ -17,7 +17,7 @@ export default function SignupForm() {
   return (
     <Box
       sx={{
-        height: { xs: "95vh", lg: "800px" },
+        height: { xs: "95vh", lg: "800px", xxl:'10vh' },
         mt: { xs: 0, lg: 1 },
         p: 3,
         bgcolor: "white",
